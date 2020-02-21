8080db68a5886021cef99bffe05bb446672f12ab
Rick <henrique.gomes5989@gmail.com>
2020-02-21 08:42:28 -0300

    Teste unitário Exercicio 1

---
+++ Exercicio 1 - Teste Unitario/Exercicio 1 - Teste Unitario/Calculator.cs
@@ -0,0 +1,43 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1___Teste_Unitario
+{
+    // Exercicio 1 - 
+    // A escola de Detroid começa com os "testes de dentro para fora do código", e buscando apenas o resultado certo de uma parte do código, para eles o mais importante é o método funcionar, nao importando como.
+    // A escola de Londres, diz que cada teste deve ser feito vendo a interação de uma classe com outras para garantir que o progama funcione como geral, para eles o mais importante é o funcionamento do metodo junto a outros métodos do programa.
+
+    // Exercicio 2a  - https://docs.microsoft.com/pt-br/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2019
+
+    public class Calculator
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+
+       
+
+        public void Add (int n1, int n2, int n3)
+        {
+            
+
+            n3 = n1 + n2;
+        }
+
+        public void Div (int n1, int n2, int n3)
+        {
+           
+            n3 = n1 / n2; 
+        }
+
+    }
+}
---
+++ Exercicio 1 - Teste Unitario/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Exercicio_1___Teste_Unitario;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int n1 = 10;
+            int n2 = 20;
+            int n3 = 0;
+            int n4 = 30;
+
+            Calculator calculator = new Calculator();
+            calculator.Add(n1, n2, n3);
+
+            if (n4 == n3)
+            {
+                Console.WriteLine("Os resultados batem");
+            } else
+            {
+                Console.WriteLine("Resultados diferentes");
+            }
+        }
+
+        public void TestMethod2()
+        {
+            int n1 = 20;
+            int n2 = 10;
+            int n3 = 0;
+            int n4 = 2;
+
+            Calculator calculator = new Calculator();
+            calculator.Div(n1, n2, n3);
+            if (n4 == n3)
+            {
+                Console.WriteLine("Os resultados batem");
+            }
+            else
+            {
+                Console.WriteLine("Resultados diferentes");
+            }
+        }
+      
+    }
+    
+}
